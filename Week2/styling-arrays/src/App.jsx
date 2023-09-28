91872a13a9c722e523e5d4594f58c7ba35657604
Emery <jemery@rtc.edu>
2023-09-28 12:17:56 -0700

    Example Complete

--- Week2/styling-arrays/src/App.jsx
+++ Week2/styling-arrays/src/App.jsx
@@ -5,6 +5,26 @@ import StyleWithProps from "./Components/StyleWithProps";
 import "./App.css";
 
 function App() {
+  const users = [
+    {
+      userName: "john_doe",
+      phoneNumber: "123-456-7890",
+      email: "john@example.com",
+      address: "123 Main Street, Cityville",
+    },
+    {
+      userName: "jane_smith",
+      phoneNumber: "987-654-3210",
+      email: "jane@example.com",
+      address: "456 Elm Street, Townsville",
+    },
+    {
+      userName: "bob_jackson",
+      phoneNumber: "555-123-4567",
+      email: "bob@example.com",
+      address: "789 Oak Street, Villageton",
+    },
+  ];
   return (
     <>
       <h2>App</h2>
@@ -20,14 +40,27 @@ function App() {
         email={"none@none.com"}
         address={"123 anywhere lane"}
       ></StyledInComponent>
-      <StyleWithProps
+      {/* <StyleWithProps
         backgroundColor={"darkBlue"}
         color={"white"}
         userName={"test user"}
         phoneNumber={"123-456-7890"}
         email={"none@none.com"}
         address={"123 anywhere lane"}
-      ></StyleWithProps>
+      ></StyleWithProps> */}
+      {/* render a UserCard for each user in the collection */}
+      {users.map((user, index) => {
+        // inside of here we want to return a component for each user
+        return (
+          <UserCard
+            key={index}
+            userName={user.userName}
+            phoneNumber={user.phoneNumber}
+            email={user.email}
+            address={user.address}
+          ></UserCard>
+        );
+      })}
     </>
   );
 }
